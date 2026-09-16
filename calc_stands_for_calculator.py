da8bfb2e7c8ffb384aabe805c51ca49a3e2f9648
Inigo <inigoagunod457@gmail.com>
2026-09-16 07:22:25 +0000

    Fixed bug

--- calc_stands_for_calculator.py
+++ calc_stands_for_calculator.py
@@ -1,7 +1,7 @@
 first_number = input("Enter first number: ")
 second_number = input("Enter second number: ")
 operator = input("Enter your operation(+, -, *, /): ")
-calculation = {first_number} {operator} {second_number}
-output = eval(calculation)
+calculation = f'{first_number} {operator} {second_number}'
+output = int(eval(calculation))
 
 print(f'The answer is {output}')
